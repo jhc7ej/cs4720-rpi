6a315bb3bd207d854074bcc8799f98a30771db3a
Justin <jhc7ej@virginia.edu>
2014-11-04 00:00:12 -0500

    Updated ms3 light code
    Updated ms3 light code, not tested for backslashes in JSON

--- rpi/code.py
+++ rpi/code.py
@@ -13,7 +13,6 @@ class index:
 	lightlist = {}
 	ids = []
 	for key, value in decode.iteritems():
-	#	lightlist = {}
 		id = ''
 		if key == 'lights':
 			for x in range (0,len(value)):
@@ -24,23 +23,9 @@ class index:
 				g = value[x]['green']
 				i = value[x]['intensity']
 				lightlist[id] = [r, b, g, i]
-			#	print lightlist				 
-		else:
-			if value == True:
-				for x in ids:
-					led.fill(Color(lightlist.get(x)[0], lightlist.get(x)[2], lightlist.get(x)[1], lightlist.get(x)[3]), x)
-					#print lightlist.get(x)[0], lightlist.get(x)[2], lightlist.get(x)[1], lightlist.get(x)[3], x
-					led.update()
-			else:
-				for x in ids:	
-					led.fill(Color(lightlist.get(x)[0], lightlist.get(x)[2], lightlist.get(x)[1], lightlist.get(x)[3]), x, x)
-					led.update()
-				
-	
-
-        #web.header('Content-Type', 'application/json')
-
-        #return "What's good World!"
+				print lightlist
+			#led.fill(Color(lightlist.get(x)[0], lightlist.get(x)[2], lightlist.get(x)[1], lightlist.get(x)[3]), x)
+			led.update()		 
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
