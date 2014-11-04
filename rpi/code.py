b8ac1564151f7a54bcd8dc19866e97f3b651f9a6
Justin <jhc7ej@virginia.edu>
2014-11-04 17:36:23 -0500

    Reverted to ms2 light code
    Changed back to original light code, since TAs are doing JSON parsing
    instead of us

--- rpi/code.py
+++ rpi/code.py
@@ -13,6 +13,7 @@ class index:
 	lightlist = {}
 	ids = []
 	for key, value in decode.iteritems():
+	#	lightlist = {}
 		id = ''
 		if key == 'lights':
 			for x in range (0,len(value)):
@@ -23,9 +24,23 @@ class index:
 				g = value[x]['green']
 				i = value[x]['intensity']
 				lightlist[id] = [r, b, g, i]
-				print lightlist
-			#led.fill(Color(lightlist.get(x)[0], lightlist.get(x)[2], lightlist.get(x)[1], lightlist.get(x)[3]), x)
-			led.update()		 
+			#	print lightlist				 
+		else:
+			if value == True:
+				for x in ids:
+					led.fill(Color(lightlist.get(x)[0], lightlist.get(x)[2], lightlist.get(x)[1], lightlist.get(x)[3]), x)
+					#print lightlist.get(x)[0], lightlist.get(x)[2], lightlist.get(x)[1], lightlist.get(x)[3], x
+					led.update()
+			else:
+				for x in ids:	
+					led.fill(Color(lightlist.get(x)[0], lightlist.get(x)[2], lightlist.get(x)[1], lightlist.get(x)[3]), x, x)
+					led.update()
+				
+	
+
+        #web.header('Content-Type', 'application/json')
+
+        #return "What's good World!"
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
