f23e209bdae519164962ead21accb1e8d0048b17
marlenakauer <mak4wd@virginia.edu>
2014-11-13 22:40:33 -0500

    open camera code

--- app/src/main/java/com/example/marlenakauer/wmbuild1/SearchbyDate.java
+++ app/src/main/java/com/example/marlenakauer/wmbuild1/SearchbyDate.java
@@ -1,6 +1,7 @@
 package com.example.marlenakauer.wmbuild1;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,6 +38,30 @@ public class SearchbyDate extends Activity {
                     displayToast();
                 }
             };
+
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            //go to next screen
+        }
+    }
+
+
 
     /** Updates the date in the TextView */
     private void updateDisplay() {
